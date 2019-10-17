7f4f74f37cdf36c8b98f540a5681faa3aab71266
abdosami1500 <44530940+abdosami1500@users.noreply.github.com>
2019-10-17 12:41:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var prefix = "+";
 var adminprefix = 'r'
 
 
-const developers = ["359108632656478208"]
+const developers = ["551422413108281345"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
